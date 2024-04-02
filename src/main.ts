301ecff4d448d0ebbdb1effb6771ba0014ca9983
IGORAOLIV <igoralmeida0105@gmail.com>
2024-04-02 17:48:26 -0300

    Integrate details page into application

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
+import { provideProtractorTestingSupport } from '@angular/platform-browser';
 import routeConfig from './app/routes';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
